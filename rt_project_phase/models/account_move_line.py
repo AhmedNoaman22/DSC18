9689052547d042e2c47e6e13bfe6356ff01182b5
mahmoudraafatahmed <mahmoudahmed21101@gmail.com>
2024-11-27 17:18:46 +0200

    rt_prohect_phase had been developed [for error in changing in account move line developed 01 271124 18.0

--- rt_project_phase/models/account_move_line.py
+++ rt_project_phase/models/account_move_line.py
@@ -9,12 +9,12 @@ class AccountMoveLine(models.Model):
     _description = 'Journal Item Inherit'
 
     phase_id = fields.Many2one(
-        'project.phase', 'Phase',)
+        'project.phase', 'Phase')
     milestone_id = fields.Many2one('project.milestone', 'Milestone')
 
 
-    @api.onchange('phase_id')
-    def _onchange_phase_id(self):
-        for rec in self:
-            rec.milestone_id = rec.phase_id.milestone_id
+    # @api.onchange('phase_id')
+    # def _onchange_phase_id(self):
+    #     for rec in self:
+    #         rec.milestone_id = rec.phase_id.milestone_id
 
