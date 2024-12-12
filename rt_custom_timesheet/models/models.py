81e41d9d9606321a5d39e062450c88ccac533f6c
AhmedNoaman <ahmed.noaman@rightechs.net>
2024-12-12 15:33:56 +0200

    [ADD] add module of rt_custom_timesheet

---
+++ rt_custom_timesheet/models/models.py
@@ -0,0 +1,76 @@
+from operator import index
+
+from odoo import models, fields, api, _
+from datetime import datetime
+from odoo.exceptions import UserError
+
+
+class AccountAnalyticLine(models.Model):
+    _inherit = 'account.analytic.line'
+
+    total_attendance_hour = fields.Float(string='Total Attendance Hour')
+    total_time_spent = fields.Float(string='Total Time Spent', compute="_compute_total_time_spent_in_day",
+                                    index=True)
+    remaining_hour = fields.Float(string='Remaining Hour', compute="_compute_remaining_hour",
+                                  index=True)
+
+    @api.depends('date')
+    def _compute_total_time_spent_in_day(self):
+        for record in self:
+            if record.date:
+                current_user_employee = self.env.user.employee_id
+                if not current_user_employee:
+                    record.total_time_spent = 0
+                    continue
+
+                start_of_day = datetime.combine(record.date, datetime.min.time())
+                end_of_day = datetime.combine(record.date, datetime.max.time())
+
+                timesheets = self.env['account.analytic.line'].search([
+                    ('date', '>=', fields.Date.to_string(start_of_day)),
+                    ('date', '<=', fields.Date.to_string(end_of_day)),
+                    ('employee_id', '=', current_user_employee.id)
+                ])
+
+                total_time_spent = 0.0
+                for timesheet in timesheets:
+                    total_time_spent += timesheet.unit_amount
+
+                record.total_time_spent = total_time_spent
+
+    @api.onchange('date')
+    def _compute_total_hours(self):
+        for record in self:
+            if record.date:
+                current_user_employee = self.env.user.employee_id
+
+                if not current_user_employee:
+                    record.total_attendance_hour = 0
+                    continue
+
+                start_of_day = datetime.combine(record.date, datetime.min.time())
+                end_of_day = datetime.combine(record.date, datetime.max.time())
+
+                attendances = self.env['hr.attendance'].search([
+                    ('check_in', '>=', fields.Datetime.to_string(start_of_day)),
+                    ('check_in', '<=', fields.Datetime.to_string(end_of_day)),
+                    ('employee_id', '=', current_user_employee.id)
+                ])
+                total_seconds = 0
+                for attendance in attendances:
+                    if attendance.check_in and attendance.check_out:
+                        print('========== attendance.worked_hours', attendance.worked_hours)
+                        total_seconds += attendance.worked_hours
+                record.total_attendance_hour = total_seconds
+
+    @api.depends('total_attendance_hour', 'total_time_spent')
+    def _compute_remaining_hour(self):
+        for record in self:
+            record.remaining_hour = record.total_attendance_hour - record.total_time_spent
+
+    @api.onchange('unit_amount')
+    def _check_time_spent(self):
+        for record in self:
+            if record.unit_amount > record.remaining_hour:
+                print('========== YOU CAN TIME GREATER THAN REMAINING =======')
+                raise UserError(_('Time Spent Greater Than Remaining'))
