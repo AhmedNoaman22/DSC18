0c5302f7fdf96f1a7e7d778c069f3659a88ecad0
mahmoudraafatahmed <mahmoudahmed21101@gmail.com>
2025-01-12 15:06:49 +0200

    elsaka_hr_penalty had been developed for Task had been finished as per its discription and per hour rate for employee based on 22-day and calculate Target hours included Absent Dsc18 developed02 Production

--- elsaka_hr_penalty/hr_penalty.py
+++ elsaka_hr_penalty/hr_penalty.py
@@ -558,14 +558,13 @@ class employee_delay(models.Model):
             else:
                 per_hour_rate = 0.0
             print(f' Per_hour_rate ======> {per_hour_rate}')
-            total_absent_hours = sum(delay.employee_delay_line.filtered(lambda r: r.type != 'absent').mapped('ded_applied'))
-            print(f' total_absent_hours ======> {total_absent_hours}')
-            total_hours_deduction = (delay.total_actual_delay + total_absent_hours) - ( delay.total_worked_hours -  delay.total_working )
+            total_hours_deduction = (delay.total_actual_delay) - ( delay.total_worked_hours -  delay.total_working )
             if total_hours_deduction < 0.0:
                 total_hours_deduction = -1 * total_hours_deduction
             print(f' total_hours_deduction ======> {total_hours_deduction}')
-
-            delay.total_hours_deduction = total_hours_deduction
+            total_absent_hours = sum(delay.employee_delay_line.filtered(lambda r: r.type != 'absent').mapped('ded_applied'))
+            print(f' total_absent_hours ======> {total_absent_hours}')
+            delay.total_hours_deduction = total_hours_deduction + total_absent_hours
             delay.target_deduction = total_hours_deduction * per_hour_rate
             delay.total_deduction = (deduction - waive) + delay.target_deduction
 
